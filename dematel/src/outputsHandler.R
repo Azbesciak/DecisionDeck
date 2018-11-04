401821b8fa8fbba62791e3335fa861922426fff7
Witold Kupś <witold.kups@interlan.pl>
2018-11-04 19:33:06 +0100

    Change alternativesAffectations to alternativesValues for dematel

--- dematel/src/outputsHandler.R
+++ dematel/src/outputsHandler.R
@@ -3,13 +3,13 @@ library(rJava)
 library(purrr)
 
 XMCDA_v2_TAG_FOR_FILENAME <- list(
-  prominence = "alternativesAffectations",
-  relation = "alternativesAffectations",
+  prominence = "alternativesValues",
+  relation = "alternativesValues",
   messages = "methodMessages"
 )
 XMCDA_v3_TAG_FOR_FILENAME <- list(
-  prominence = "alternativesAssignments",
-  relation = "alternativesAssignments",
+  prominence = "alternativesValues",
+  relation = "alternativesValues",
   messages = "programExecutionResult"
 )
 xmcda_v3_tag <- function(outputName) XMCDA_v3_TAG_FOR_FILENAME[[outputName]]
@@ -19,7 +19,7 @@ xmcda_v2_tag <- function(outputName) XMCDA_v2_TAG_FOR_FILENAME[[outputName]]
 assignAlternatives <- function(values, programExecutionResult) {
   xmcdaResult <- .jnew("org/xmcda/XMCDA")
   tmp <- handleException(
-  function() putAlternativesAssignments(xmcdaResult, values),
+  function() putAlternativesValues(xmcdaResult, values),
     programExecutionResult,
     humanMessage = "Could not put overall values in tree, reason: "
   )
