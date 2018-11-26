87685a06914b1bb33ce942f8d0654409e43e28f7
Witold Kupś <witold.kups@interlan.pl>
2018-11-26 19:38:33 +0100

    Add influence arrows for dematel diagram

--- dematel/src/dematel.R
+++ dematel/src/dematel.R
@@ -1,14 +1,23 @@
-displayDematelPlot <- function(x, y, df) {
+displayDematelPlot <- function(x, y, df, T, alpha) {
     plot(x, y,
         type = 'p',
         main = "DEMATEL",
         sub = "The cause and effect diagram",
         xlab = "R + C",
         ylab = "R - C",
-        cex = 0.5
+        cex = 2,
+
     )
-    text(x, y, df$Criterion, pos=4, col="red")
+    colors = c("#FF5722", "#FFC107", "#8BC34A", "#795548", "#9C27B0", "#03A9F4", "#009688", "#3F51B5")
+    text(x, y, df$Criterion, pos=2, col=colors)
     abline(h = mean(y), v = mean(x))
+    for (i in 1: nrow(T))
+        for (j in 1: ncol(T))
+            if (T[i,j] > alpha && i != j) {
+                arrows(x[j],y[j],x[i],y[i],
+                length = 0.15, angle = 45,
+                code = 1, col = colors[i %% length(colors)])
+            }
     View(df)
 }
 
@@ -46,11 +55,11 @@ dematel = function(Z) {
 
     x = R + C; y = R - C
     # step 5
-    alpha = mean(T) # TODO filter...?
+    alpha = mean(T)
     Criterion = colnames(D)
     df = data.frame(Criterion, R, C, x, y)
     colnames(df) <- c("Criterion", "R", "C", "R + C", "R - C")
-    displayDematelPlot(x, y, df)
+    displayDematelPlot(x, y, df, T, alpha)
     df
 }
 #
