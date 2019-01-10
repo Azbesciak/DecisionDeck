081e6b7c77aa06382a40bd00808062283a2848d0
Witold Kupś <witold.kups@interlan.pl>
2019-01-10 19:58:38 +0100

    Add fail-fast in vikor-compromises if Q vector would have invalid value

--- vikor-compromises/src/vikorCompromises.R
+++ vikor-compromises/src/vikorCompromises.R
@@ -1,58 +1,77 @@
+# It is not an optimization - R will evaluate `(1 - v) * (R - minR) / (maxR - minR)`
+# even if (1-v) == 0, so in result we will receive NaN.
+# Same applies to the case with v == 0.
+.computeQValue = function(S, R, v) {
+	minS = min(S)
+	maxS = max(S)
+	if (minS == maxS && v != 0)
+	stop("Could not compute Q vector: extremes in vector S are equal")
+
+	minR = min(R)
+	maxR = max(R)
+	if (minR == maxR && v != 1)
+	stop("Could not compute Q vector: extremes in vector R are equal")
+
+	if (v == 1)
+		(S - minS) / (maxS - minS)
+	else if (v == 0)
+		(R - minR) / (maxR - minR)
+	else
+		v * (S - minS) / (maxS - minS) + (1 - v) * (R - minR) / (maxR - minR)
+}
+
 vikorCompromises = function(S, R, v) {
-    if (missing(S) || missing(R) || missing(v))
-        stop("usage:
-            S - weighted and normalized Manhattan distance ranking by minimum value.
+	if (missing(S) || missing(R) || missing(v))
+	stop("usage:
+            S - weighted and normalized Manhattan distance ranking by minimum value
             R - weighted and normalized Chebyshev distance ranking by minimum value
             v - weight of the strategy of S and R [0,1].
         ")
-    if (is.null(S))
-        stop("'S' is null")
-    if (is.null(R))
-        stop("'R' is null")
-    if (is.null(v))
-        stop("'v' is null")
-    if (!is.vector(S) || !all(S >= 0))
-        stop("'S' must be a non-negative values vector")
-    if (!is.vector(R) || !all(R >= 0))
-        stop("'R' must be a non-negative values vector")
-    if (v < 0 || v > 1)
-        stop("A value for 'v' (veto) in [0,1] should be provided")
+	if (is.null(S))
+	stop("'S' is null")
+	if (is.null(R))
+	stop("'R' is null")
+	if (is.null(v))
+	stop("'v' is null")
+	if (! is.vector(S) ||
+		! all(S >= 0) ||
+		! all(is.numeric(S)))
+	stop("'S' must be a non-negative values vector")
+	if (! is.vector(R) ||
+		! all(R >= 0) ||
+		! all(is.numeric(R)))
+	stop("'R' must be a non-negative values vector")
+	if (v < 0 || v > 1 || ! is.numeric(v))
+	stop("A numeric value for 'v' (veto) in [0,1] should be provided")
 
-    S = S[order(names(S))]
-    R = R[order(names(R))]
-    #3. Q index
-    Q = v * (S - min(S)) / (max(S) - min(S)) + (1 - v) * (R - min(R)) / (max(R) - min(R))
+	S = S[order(names(S))]
+	R = R[order(names(R))]
 
-    #4. Checking if Q is valid
-    isQValid = Q != "NaN" && Q != "Inf"
-    QRanking = if (isQValid) {
-        rank(Q, ties.method = "first")
-    } else {
-        rep("-", length(R))
-    }
-    #5. Ranking the alternatives
-    compromiseSolution = if (isQValid) {
-        sortedQRank = names(sort(QRanking))
-        sortedSRank = names(sort(rank(S, ties.method = "first")))
-        sortedRRank = names(sort(rank(R, ties.method = "first")))
-        DQ = 1 / (length(R) - 1)
-        isAcceptableAdvantage = Q[[sortedQRank[2]]] - Q[[sortedQRank[1]]] >= DQ
-        isAcceptableStability = sortedQRank[1] == sortedSRank[1] || sortedQRank[1] == sortedRRank[1]
-        if (!isAcceptableStability && isAcceptableAdvantage) {
-            sortedQRank[1:2]
-        } else if (!isAcceptableAdvantage) {
-            names(Filter(function(X) X - Q[sortedQRank[1]] < DQ, Q))
-        } else {
-            sortedQRank[1]
-        }
-    } else {
-        c()
-    }
-    list(
-        ranking = data.frame(
-            Q = Q,
-            Ranking = QRanking
-        ),
-        compromiseSolutions = compromiseSolution
-    )
+	#3. Q index
+	Q = .computeQValue(S, R, v)
+	#4. Checking if Q is valid
+	if (Q == "NaN" || Q == "Inf")
+		stop("Could not compute compromises: Q vector contains INF or NaN")
+	QRanking = rank(Q, ties.method = "first")
+	#5. Ranking the alternatives
+	sortedQRank = names(sort(QRanking))
+	sortedSRank = names(sort(rank(S, ties.method = "first")))
+	sortedRRank = names(sort(rank(R, ties.method = "first")))
+	DQ = 1 / (length(R) - 1)
+	isAcceptableAdvantage = Q[[sortedQRank[2]]] - Q[[sortedQRank[1]]] >= DQ
+	isAcceptableStability = sortedQRank[1] == sortedSRank[1] || sortedQRank[1] == sortedRRank[1]
+	compromiseSolution = if (! isAcceptableStability && isAcceptableAdvantage) {
+		sortedQRank[1 : 2]
+	} else if (! isAcceptableAdvantage) {
+		names(Filter(function(X) X - Q[sortedQRank[1]] < DQ, Q))
+	} else {
+		sortedQRank[1]
+	}
+	list(
+		ranking = data.frame(
+			Q = Q,
+			Ranking = QRanking
+		),
+		compromiseSolutions = compromiseSolution
+	)
 }
