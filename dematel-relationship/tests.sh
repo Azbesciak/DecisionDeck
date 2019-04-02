29cc06b02277e4dc7d73fe7b50a30f1b194d1e9f
Sébastien Bigaret <sebastien.bigaret@telecom-bretagne.eu>
2019-04-02 11:13:38 +0200

    Complement bca399d4: tests for dematel-*/vikor-* canonicalise the xmcda files

--- dematel-relationship/tests.sh
+++ dematel-relationship/tests.sh
@@ -10,13 +10,37 @@ elif [ "${1}" != "--v2" ] && [ "${1}" != "--v3" ]; then
 fi
 version=${1#*v}
 NB_TESTS=$(find tests -maxdepth 1 -type d -regex '.*/in[0-9]*\.v'"${version}"'$' | wc -l)
+
+c14n_dir()
+{
+    find "${1:?}" -type f -name '*.xml' -print0 | \
+        while IFS= read -r -d $'\0' xmlfile
+    do
+        if xmllint --c14n "$xmlfile" > "${xmlfile}".c14n; then
+            mv "${xmlfile}".c14n "${xmlfile}"
+        else
+            rm "${xmlfile}".c14n
+        fi
+    done
+}
+
 mkdir -p tests_tmp
 for i in $(seq 1 "${NB_TESTS}"); do
     IN="tests/in${i}.v${version}"
     REFERENCE_OUT="tests/out${i}.v${version}"
     OUT=$(mktemp --tmpdir=. -d tests_tmp/out.XXX)
+    mkdir "${OUT}"/expected "${OUT}"/real
+    cp -Rp "${REFERENCE_OUT}"/. "${OUT}"/expected
+
+    REFERENCE_OUT="${OUT}"/expected
+    OUT="${OUT}"/real
+
     echo "${IN}"
     ${CMD} "--v${version}" "${IN}" "${OUT}"
+
+    c14n_dir $REFERENCE_OUT
+    c14n_dir $OUT
+
     diff -x README -ruBw "${REFERENCE_OUT}" "${OUT}"
     ret_diff=$?
     if [ $ret_diff -ne 0 ]; then
